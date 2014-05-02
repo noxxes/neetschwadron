388100db78a2414fa9cf5f00d8ac7c544c32a69a
noxxes <thelow@live.de>
2014-05-02 14:04:45 +0200

    reset
    ein reset auf 0 als test

--- main.cpp
+++ main.cpp
@@ -1,71 +1,11 @@
 #include <iostream>
 #include <string>
-#include "Spieler.h"
-#include "Gegner.h"
-#include "Kampf.h"
 //#include "Item.h"
 using namespace std;
 //int argc, char *argv[]
 int main ()
 {
-/*
-	Item gaga;
-	gaga.setKind("Schwert");
-	gaga.init();
-	
-	//Kampf 
-
-	Kampf k;
-	//Test für die Schleife und regelt die angriffsphasen
-	int counter;
-	int down = 0;
-	counter = 0;
-	do
-	{
-		cout << "Dein Gegner hat noch: " << gLive << endl;
-		cout << "Du hast noch: " << pLive << endl;
-		
-		if(counter%2 == 0)
-		{
-			
-			cout << "--------------" << endl;
-			cout << "Der Gegner Greift an" << endl;
-			dmg = k.angriff(g.getAngriff(),p.getDev());
-			cout << "Schaden: " << dmg << endl;
-			pLive = pLive-dmg;
-			p.setLive(pLive);
-		}else
-		{	
-			
-			cout << "--------------" << endl;
-			cout << "Der Spieler Greift an" << endl; 
-			dmg = k.angriff(p.getAngriff(),g.getDev());
-			cout << "Schaden: " << dmg << endl;
-			gLive = gLive-dmg;
-			g.setLive(gLive);
-		}
-		
-		counter = counter+1;
-		
-		//cout << counter << endl;	
-		if(g.getLive() <=0)
-		{
-			cout <<"####################" << endl;
-			cout <<"Der Gegner ist Tot "<< endl;
-			down = 1;
-		}
-		if(p.getLive() <= 0)
-		{
-			cout <<"####################" << endl;
-			cout << "Der Spieler ist Tot. Du NOOB!!!!!!!!!!" << endl;
-			int down = 1;
-		}	
-		//cout << "Lauf: " << counter << endl;
-	}while(down == 0);
-	//counter falls entlosschleife
-	//while(counter <=5 );
-	
-*/
+    cout << "test" << endl;
 
 	return 0;
 }
